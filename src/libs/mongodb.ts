4030cf4b83135196105723460df62b282b4285c3
Matt-PY-GH <mateldraka@gmail.com>
2025-07-12 01:21:30 -0300

    little change

--- src/libs/mongodb.ts
+++ src/libs/mongodb.ts
@@ -1,20 +1,21 @@
 import mongoose from "mongoose";
 
-const {DB_CONNECTION} = process.env;
+const MONGODB_URI = process.env.NODE_ENV === "production"
+  ? process.env.MONGODB_URI
+  : process.env.DB_CONNECTION;
 
-if(!DB_CONNECTION) throw new Error("ERROR TO CONNECT TO THE DATABASE");
+if (!MONGODB_URI) throw new Error("ERROR TO CONNECT TO THE DATABASE");
 
-export const connectDB = async () =>{
-    try{
-        const {connection} = await mongoose.connect(DB_CONNECTION);
-        if(connection.readyState === 1){
-            console.log("Database connected succesfully!")
-            return Promise.resolve(true)
-        }
-
-    }catch(err){
-        console.log(err);
-        return Promise.reject(false)
+export const connectDB = async () => {
+  try {
+    const { connection } = await mongoose.connect(MONGODB_URI);
+    if (connection.readyState === 1) {
+      console.log("Database connected succesfully!");
+      return Promise.resolve(true);
     }
-    
-}
+  } catch (err) {
+    console.log(err);
+    return Promise.reject(false);
+  }
+};
+
