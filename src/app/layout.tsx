df3f38fef31d0429588a97358034d3e8c846acbe
Matt-PY-GH <mateldraka@gmail.com>
2025-07-12 15:19:23 -0300

    o image added and better SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,11 +15,37 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Devter",
-  description: "Organize your projects with Devlter, your developer shelter.",
+  description: "Devter is your digital shelter for organizing code, ideas, bugs, notes and more.",
+  keywords: ["Devter", "DevTracker", "developer tools", "project organization", "coding notes"],
+  authors: [{ name: "Mateo Delgado", url: "https://devter.dev" }],
+  creator: "Mateo Delgado",
+  openGraph: {
+    title: "Devter – Your developer shelter",
+    description: "Organize your dev life with tools like Backlog, CMD, Paint, and more.",
+    url: "https://devter.dev",
+    siteName: "Devter",
+    images: [
+      {
+        url: "https://devter.dev/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Devter - Developer Shelter",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Devter",
+    description: "Organize your dev life with Devter.",
+    images: ["https://devter.dev/og-image.png"],
+  },
   icons: {
     icon: "/favicon.ico",
-  }
+  },
 };
+
 
 export default function RootLayout({
   children,
