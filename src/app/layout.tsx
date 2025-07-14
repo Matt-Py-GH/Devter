6414b970e2515ebc8bce4c9612bc897f37c383d5
Matt-PY-GH <mateldraka@gmail.com>
2025-07-13 21:35:57 -0300

    icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,8 +63,8 @@ export default function RootLayout({
 
         {/* Favicons y manifest */}
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.ico" />
         <link rel="manifest" href="/site.webmanifest" />
 
         {/* Optimización de fuentes */}
